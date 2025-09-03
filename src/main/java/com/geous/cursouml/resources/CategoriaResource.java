ab11a21059ba9d393c1e55a41c464102cedef993
Anticlei <anticlei@gmail.com>
2025-09-02 21:46:06 -0300

    Teste primeira classe dominio Categoria

--- src/main/java/com/geous/cursouml/resources/CategoriaResource.java
+++ src/main/java/com/geous/cursouml/resources/CategoriaResource.java
@@ -1,15 +1,24 @@
 package com.geous.cursouml.resources;
 
+import com.geous.cursouml.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listarCategorias(){
-        return "REST está funcionando";
+    public List<Categoria> listarCategorias(){
+        Categoria cat1 = new Categoria(1,"Informática");
+        Categoria cat2 = new Categoria(2,"Escritório");
+        List<Categoria> listaCategorias = new ArrayList<>();
+        listaCategorias.add(cat1);
+        listaCategorias.add(cat2);
+        return listaCategorias;
     }
 }
