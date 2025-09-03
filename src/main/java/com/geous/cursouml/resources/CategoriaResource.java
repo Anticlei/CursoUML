d8b98270f6155b72e6202caf1294599e60386408
Anticlei <anticlei@gmail.com>
2025-09-02 21:22:06 -0300

    REST testado e funcionando

---
+++ src/main/java/com/geous/cursouml/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.geous.cursouml.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String listarCategorias(){
+        return "REST está funcionando";
+    }
+}
